f892cfc92ecc9786c9daeb5f5af6992530852174
ccrozz <crozier.coleman@gmail.com>
2023-11-06 18:12:46 -0500

    Change var name

--- server/app.py
+++ server/app.py
@@ -9,8 +9,8 @@ CORS(app)
 def get_stock_data():
     symbol = request.args.get('symbol')
     stock = yf.Ticker(symbol)
-    stock_info = stock.info
-    return jsonify(stock_info)
+    stock_data = stock.info
+    return jsonify(stock_data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
